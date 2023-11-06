8de316a9275cf35251af636687ba47fb9bcd9f5c
Seemabhattarai <simabhattaraibca077@indrenicollege.edu.np>
2023-11-06 16:54:34 +0545

    toohard

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,48 +1,75 @@
 import React from 'react'
 import Image from '../assets/IMG-20221126-WA0008.jpg'
+import { useState } from 'react'
+import {  ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Contact = () => {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  console.log(firstname, lastname, phone, address, email, message);
+  const handleClick = (e) => {
+    e.preventDefault();
+    // if (firstname && lastname && phone && address && email && message) {
+      toast("Form Submit Sucessfully");
+      setFirstname("");
+      setLastname("");
+      setPhone("");
+      setAddress("");
+      setEmail("");
+      setMessage("");
+    // }
+
+  }
   return (
     <>
-    <div className='flex gap-8 ml-8 mt-8 p-8'>
-      <form className='border  p-8  w-[60%]'>
-      
-        <h1 className='text-center font-bold mb-4 text-[24px] text-blue-600'>Contact </h1>
-        <div className='grid grid-cols-2 gap-2'>
-          <div className='flex flex-col'>
-            <label> firstname </label>
-            <input className='px-3 py-2 border w-full focus:outline-none focus:ring focus:ring-blue-500 rounded-md' type='text' name='firstname' placeholder='firstname' />
-          </div>
-          <div className='flex flex-col'>
-            <label> Lastname </label>
-            <input className='px-3 py-2 border w-full focus:outline-none focus:ring focus:ring-blue-500 rounded-md' type='text' name='lastname' placeholder='lastname' />
-          </div>
-          <div className='flex flex-col'>
-            <label> phone </label>
-            <input className='px-3 py-2 border w-full focus:outline-none focus:ring focus:ring-blue-500 rounded-md' type='number' name='phone' placeholder='phone' />
-          </div>
-          <div className='flex flex-col'>
-            <label> address </label>
-            <input className='px-3 py-2 border w-full focus:outline-none focus:ring focus:ring-blue-500 rounded-md' type='text' name='address' placeholder='address' />
-          </div>
-          </div>
-          <div className='flex flex-col'>
-            <label> email </label>
-            <input className='px-3 py-2 border w-full focus:outline-none focus:ring focus:ring-blue-500 rounded-md' type='text' name='abc@gmail.com' placeholder='abc@gmail.com' />
+      <div className='flex gap-8 m-8'>
+        <ToastContainer />
+        <form action="" className='border-2 w-[50%] p-4 mt-4 ml-2 mb-4 bg-slate-100 '>
+          <div className='container  '>
 
-          </div>
-          <div className='flex flex-col'>
-            <label> message </label>
-            <textarea className='px-3 py-2 border w-full focus:outline-none focus:ring focus:ring-blue-500 rounded-md'  name="" id="" cols="30" rows="5"></textarea>
+            <h1 className='text-2xl text-center font-bold mb-4 '>Contact Us</h1>
+            <div className='grid grid-cols-2 gap-2'>
+              <div>
+                <label htmlFor="name" >Firstname</label>
+                <input onChange={e => setFirstname(e.target.value)} value={firstname} className='px-3 py-2 border w-full focus:outline-none focus:ring-blue-500 rounded-md' type="text" name='firstname' placeholder='firstname' required />
+              </div>
+              <div>
+                <label htmlFor="name" >Lastname</label>
+                <input onChange={e => setLastname(e.target.value)} value={lastname} className='px-3 py-2 border w-full focus:outline-none focus:ring-blue-500 rounded-md' type="text" name='lastname' placeholder='lastname' required />
+              </div>
+              <div>
+                <label htmlFor="phone" >Phone</label>
+                <input onChange={e => setPhone(e.target.value)} value={phone} className='px-3 py-2 border w-full focus:outline-none focus:ring-blue-500 rounded-md' type="number" name='phone' placeholder='phone' required />
+              </div>
+              <div >
+                <label htmlFor="address" >Address</label>
+                <input onChange={e => setAddress(e.target.value)} value={address} className='px-3 py-2 border w-full focus:outline-none focus:ring-blue-500 rounded-md' type="text" name='adresss' placeholder='address' required />
+              </div>
             </div>
-             <div className='flex  justify-center '>
-            <button className=' flex justify-center items-center bg-blue-600 text-white px-4 py-2 rounded-full hover:bg-white hover:text-black text-center mt-4 w-fit'>Submit</button>
-             </div>
+            <div>
+              <label htmlFor="email" className='block text-sm font-semibold mb-2'>Email</label>
+              <input onChange={e => setEmail(e.target.value)} value={email} className='px-3 py-2 border w-full focus:outline-none focus:ring-blue-500 rounded-md' type="email" name='email' placeholder='email@gmail.com' required />
+            </div>
+            <div >
+              <label htmlFor="message" className='block text-sm font-semibold mb-2'>Message</label>
+              <textarea onChange={e => setMessage(e.target.value)} value={message} className='px-3 py-2 border w-full focus:outline-none focus:ring-blue-500 rounded-md' name="message" id="" cols="30" rows="5" required></textarea>
+            </div>
+            <div className='text-center'>
+              <button type='submit' onClick={handleClick} className='bg-red-500 text-white font-semibold px-4 py-2 rounded hover:bg-green-700 ' >
+                Submit
+              </button>
+            </div>
+          </div>
+        </form >
 
-          
-      </form>
 
-      <img className="h-fit w-[40%] object-cover rounded-2xl"  src={Image} alt=""/>
+        <img className=" w-[40%] object-cover rounded-2xl" src={Image} alt="" />
       </div>
     </>
   )
